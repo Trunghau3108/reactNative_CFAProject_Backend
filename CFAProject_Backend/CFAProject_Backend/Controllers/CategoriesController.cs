2e76688e4339e80db905950097bd60c42dcedd66
TrungHau3108 <trunghau3108@gmail.com>
2023-07-05 15:36:26 +0700

    test

---
+++ CFAProject_Backend/CFAProject_Backend/Controllers/CategoriesController.cs
@@ -0,0 +1,42 @@
+﻿using CFAProject_Backend.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Xml;
+
+namespace CFAProject_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly CFAProjectContext _context;
+        public CategoriesController(CFAProjectContext ctx)
+        {
+            _context = ctx;
+        }
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Categories.ToList());
+        }
+
+        [HttpPost]
+        /*public IActionResult GetCategoryById()
+        {
+            int id;
+            if (!int.TryParse(Request.Body.ToString(), out id))
+            {
+                return BadRequest("Invalid ID"); // Trả về HTTP 400 Bad Request nếu ID không hợp lệ
+            }
+
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound(); // Trả về HTTP 404 Not Found nếu không tìm thấy danh mục với ID tương ứng
+            }
+
+            return Ok(category); // Trả về danh mục tìm thấy dưới dạng JSON
+        }*/
+    }
+}
---
+++ CFAProject_Backend/CFAProject_Backend/Program.cs
@@ -0,0 +1,29 @@
+using CFAProject_Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CFAProjectContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyApp")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
